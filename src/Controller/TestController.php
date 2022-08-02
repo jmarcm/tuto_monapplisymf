22c59b3b2e6b5a613a90f011a0c1c5500aae1ad6
Jean-Marc (PC) <mjean_marc@hotmail.com>
2022-08-02 12:03:14 +0200

    Response: json response

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,8 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,21 +12,13 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="app_test")
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
 
-        // L'objet Request est instancié automatiquement
-        echo $request->query->get('info') . '<br/>';
         // return $this->render('test/index.html.twig', [
         //     'controller_name' => 'TestController',
         // ]);
-        $response = new Response();
-        $response->setContent('Bienvenue dans Symfony');
-        $response->headers->set('Content-Type', 'text/plain');
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-        $response->setCharset('ISO-8859-1');
-
-        $response->headers->setCookie(Cookie::create('foo', 'bar'));
+        $response = new JsonResponse(['data', 123]);
         return $response;
     }
 }
