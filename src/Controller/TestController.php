cc9e4ad5b7a52c4cda4ada17bbdc85ae981392ee
Jean-Marc (PC) <mjean_marc@hotmail.com>
2022-08-02 17:35:56 +0200

    Response: streamed ?

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
@@ -12,13 +13,20 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="app_test")
      */
-    public function index(): Response
+    public function index()
     {
 
         // return $this->render('test/index.html.twig', [
         //     'controller_name' => 'TestController',
         // ]);
-        $response = new JsonResponse(['data', 123]);
-        return $response;
+        $response = new StreamedResponse();
+        $response->setCallback(function () {
+            var_dump('Hello World');
+            flush();
+            sleep(2);
+            var_dump('Hello World');
+            flush();
+        });
+        $response->send();
     }
 }
