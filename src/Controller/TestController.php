9f6c2d460eb6163f42c9754cba0b1da3e11f3d33
Jean-Marc (PC) <mjean_marc@hotmail.com>
2022-08-02 17:46:01 +0200

    Les variables de session

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,20 +14,30 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="app_test")
      */
-    public function index()
+    public function index(Request $request)
     {
 
-        // return $this->render('test/index.html.twig', [
-        //     'controller_name' => 'TestController',
-        // ]);
-        $response = new StreamedResponse();
-        $response->setCallback(function () {
-            var_dump('Hello World');
-            flush();
-            sleep(2);
-            var_dump('Hello World');
-            flush();
-        });
-        $response->send();
+        // session_start()
+        $session = $request->getSession();
+        $session->set('nom_user', 'jmarcm');
+        
+        $url = $this->generateUrl('redirection');
+        return $this->redirect($url);
+    }
+
+
+    /**
+     * @Route("/redirection", name="redirection")
+     */
+    public function redirection(Request $request): Response
+    {
+
+        // récupération de la session
+        $session = $request->getSession();
+
+        // récupération du nom de l'utilisateur
+        $nom_user = $session->get('nom_user');
+
+        return new Response("Ici redirection ; variable de session : $nom_user");
     }
 }
