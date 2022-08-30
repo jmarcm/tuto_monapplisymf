57c298dee158ffe906ac05ee433312bd0f6aa5ba
Jean-Marc (PC) <mjean_marc@hotmail.com>
2022-08-30 16:07:59 +0200

    Form : Formulaire externe -- création

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, ['label' => 'Nom du produit :'])
+            ->add('prix', NumberType::class, ['label' => 'Prix :'])
+            ->add('quantite', NumberType::class, ['label' => 'Quantité :'])
+            ->add('rupture', CheckboxType::class, ['label' => 'Rupture de stock', 'required' => false])
+            ->add('lienImage', FileType::class, ['label' => 'Image :', 'required' => false, 'data_class' => null, 'empty_data', 'Aucune image'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
