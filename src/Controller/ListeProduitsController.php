186249d81131d75839cede7cf988aa909d36559d
Jean-Marc <mjean_marc@hotmail.com>
2022-12-16 17:45:10 +0100

    Try to fix issues

--- src/Controller/ListeProduitsController.php
+++ src/Controller/ListeProduitsController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 use App\Entity\Produit;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ListeProduitsController extends AbstractController
 {
+
+    function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
     /**
      * @Route("/liste", name="liste")
      */
@@ -18,7 +26,8 @@ class ListeProduitsController extends AbstractController
     {
 
         // On accède aux produits via le Respository
-        $listeProduits = $doctrine->getRepository(Produit::class)->findAll();
+        $produitsRepository = $this->em->getRepository(Produit::class);
+        $listeProduits = $produitsRepository->findAll();
 
         return $this->render('liste_produits/index.html.twig', [
             'listeproduits' => $listeProduits
--- src/DataFixtures/ProduitFixtures.php
+++ src/DataFixtures/ProduitFixtures.php
@@ -6,6 +6,8 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use App\Data\ListeProduits;
+
+// Pour récupérer la classe Produit
 use App\Entity\Produit;
 
 class ProduitFixtures extends Fixture
@@ -16,8 +18,10 @@ class ProduitFixtures extends Fixture
         // $manager->persist($product);
         foreach (ListeProduits::$mesProduits as $monProduit) {
 
+            // Pour instancier des entités
             $produit = new Produit();
 
+            // Insertion des propriétés
             $produit->setNom($monProduit['nom']);
             $produit->setPrix($monProduit['prix']);
             $produit->setQuantite($monProduit['quantite']);
