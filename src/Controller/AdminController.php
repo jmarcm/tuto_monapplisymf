a2c906e76ccc8a354e3eca055441cea4be7b98d7
Jean-Marc <mjean_marc@hotmail.com>
2023-01-31 15:36:48 +0100

    Route control in controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,6 +13,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
