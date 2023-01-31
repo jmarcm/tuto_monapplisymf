8a0b98318eeb8b5e1d4afa70adf7a056f00514ff
Jean-Marc <mjean_marc@hotmail.com>
2023-01-31 15:59:43 +0100

    Create AccessDeniedHandler()

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler extends AbstractController implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return $this->redirect($this->generateUrl('app_login'));
+    }
+}
