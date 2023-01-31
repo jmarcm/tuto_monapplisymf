e7973d38a2e5ad1f823dd57cbe8081a8753636a2
Jean-Marc <mjean_marc@hotmail.com>
2023-01-31 16:23:00 +0100

    Accès contrôleur avec Security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,9 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 /**
- * @Route("/admin")
+ * @Security("is_granted('ROLE_ADMIN')")
  */
 class AdminController extends AbstractController
 {
